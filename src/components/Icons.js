c8620a92633ce343164b80e27b895dd4a2dbd47f
cyvvang <107595056+cyvvang@users.noreply.github.com>
2024-04-04 23:07:01 -0400

    Delete src directory

--- src/components/Icons.js
+++
@@ -1,9 +0,0 @@
-const Icons = ({onClickHandler, value, img}) => {
-  return (
-    <div className="pfp-btn" onClick={onClickHandler} value={value}>
-      <img src={img}/>
-    </div>
-  );
-}
-
-export default Icons;
